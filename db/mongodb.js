d02456269fd410823e0d04ee911339a3ba2bd480
renu-singhh <m21renusingh@gmail.com>
2024-07-22 17:00:37 +0530

    initial commit

---
+++ db/mongodb.js
@@ -0,0 +1,22 @@
+const mongoose = require('mongoose');
+
+const connectDB = (url) => {
+    return mongoose.connect(url).then(() => {
+        console.log("Successfully connected to DB");
+    }).catch((err) => {
+        console.error("Error connecting to the DB", err);
+        process.exit(1);
+    });
+};
+
+const disconnectDB = () => {
+    mongoose.connection.close((err) => {
+        if (err) {
+            console.error('Error closing MongoDB connection:', err);
+        } else {
+            console.log('MongoDB connection closed successfully');
+        }
+    });
+};
+
+module.exports = { connectDB, disconnectDB };
